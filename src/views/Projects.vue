c977eb23b2ba75e71a0f5d1c99d10ad68313f831
bitbeast18 <pro.bbcom18@gmail.com>
2020-01-16 02:23:48 +0530

    Completed everything

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,44 +1,47 @@
 <template>
-  <v-container>
-    <v-window v-model="onboarding" vertical>
-      <v-window-item v-for="project in projects" :key="project.name">
-        <v-row>
-          <v-col cols="12">
-            <v-card height="75vh" elevation="12">
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-card-title>{{ project.name }}</v-card-title>
-                  <v-card-subtitle>{{ project.text }}</v-card-subtitle>
-                  <v-card-text>
-                    <div>{{ project.text }}</div>
-                    <div class="subtitle">{{ project.tools }}</div>
-                  </v-card-text>
+  <v-container class="mt-10 rounded white">
+    <v-card flat class="transparent">
+      <v-window v-model="onboarding" vertical>
+        <v-window-item v-for="project in projects" :key="project.name">
+          <v-container>
+            <v-row class="d-flex justify-space-around">
+              <v-col cols="4" class="d-flex flex-column">
+                <div class="flex-grow-1">
+                  <v-card-title>{{project.name}}</v-card-title>
+                  <v-card-text>{{project.text}}</v-card-text>
                 </div>
-                <v-img :src="project.image"></v-img>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-window-item>
-    </v-window>
+                <div class="mt-auto pa-2">
+                  <v-btn class="mr-4">View source</v-btn>
+                  <v-btn>View demo</v-btn>
+                </div>
+              </v-col>
 
-    <v-card-actions class="justify-space-between">
-      <v-btn fab x-small @click="prev">
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
+              <v-col cols="7" class="ma-0 pa-0">
+                <v-img contain :src="project.image"></v-img>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-window-item>
+      </v-window>
 
-      <v-item-group v-model="onboarding" class="text-center" mandatory>
-        <v-item v-for="n in length" :key="`btn-${n}`" v-slot:default="{ active, toggle }">
-          <v-btn :input-value="active" icon @click="toggle">
-            <v-icon>mdi-record</v-icon>
-          </v-btn>
-        </v-item>
-      </v-item-group>
+      <v-card-actions class="mt-8 justify-space-between">
+        <v-btn fab x-small @click="prev">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
 
-      <v-btn fab x-small @click="next">
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-card-actions>
+        <v-item-group v-model="onboarding" class="text-center" mandatory>
+          <v-item v-for="n in length" :key="`btn-${n}`" v-slot:default="{ active, toggle }">
+            <v-btn :input-value="active" icon @click="toggle">
+              <v-icon>mdi-record</v-icon>
+            </v-btn>
+          </v-item>
+        </v-item-group>
+
+        <v-btn fab x-small @click="next">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
@@ -49,21 +52,21 @@ export default {
     projects: [
       {
         name: "Sprint",
-        image: "/project1.jpg",
+        image: "/project1.png",
         text: "A text editor for competitive programmers."
       },
       {
         name: "Personal Portfolio",
-        image: "/project2.jpg",
+        image: "/project2.png",
         text: "Personal Website for showcasing myself."
       },
       {
         name: "Sandbox",
-        image: "/project3.jpg",
+        image: "/project1.png",
         text: "A tool for proctured coding assessments."
       }
     ],
-    length: 10
+    length: 3
   }),
 
   methods: {
