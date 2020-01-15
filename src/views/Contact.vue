70d0fb68a4514815c4b02f813ae1957694267fee
bitbeast18 <pro.bbcom18@gmail.com>
2020-01-15 23:39:05 +0530

    Completed Contact tab

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,116 @@
 <template>
-    <p> Contact </p>
+  <v-container fill-height>
+    <v-row class="justify-space-between">
+      <v-col cols="5" class="flex-grow-0">
+        <v-card class="px-4 d-flex ma-4 elevation-10" v-for="item in items" :key="item.name">
+          <v-icon large>{{item.icon}}</v-icon>
+          <div>
+            <v-card-title>{{item.name}}</v-card-title>
+            <v-card-subtitle>{{item.type}}</v-card-subtitle>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6" class=" elevation-15 flex-grow-1 pa-5 white rounded">
+        <v-form ref="form">
+          <v-container class>
+            <v-row dense>
+              <v-col cols="12">
+                <v-text-field
+                  :rules="nameRules"
+                  required
+                  v-model="first"
+                  label="First Name"
+                  outlined
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field :rules="nameRules" required v-model="last" label="Last Name" outlined></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field :rules="emailRules" required v-model="mail" label="Email" outlined></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-textarea
+                  v-model="purpose"
+                  no-resize
+                  outlined
+                  required
+                  :rules="textRules"
+                  rows="4"
+                  row-height="15"
+                  label="Why do you want to contact?"
+                ></v-textarea>
+              </v-col>
+              <v-card-actions>
+                <v-btn @click="reset" class="mr-5">Reset</v-btn>
+                <v-btn @click="submit">Submit</v-btn>
+              </v-card-actions>
+            </v-row>
+          </v-container>
+          <v-snackbar v-model="snackbar">
+              {{ msg }} <v-btn dark small @click="snackbar = false"> Close </v-btn>
+          </v-snackbar>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: "pro.bbcom18@gmail.com",
+          type: "Email",
+          icon: "fas fa-envelope"
+        },
+        {
+          name: "/bitbeast18",
+          type: "Github",
+          icon: "fab fa-github"
+        },
+        {
+          name: "bitbeast18.github.io",
+          type: "Website",
+          icon: "fas fa-globe"
+        }
+      ],
+      snackbar: false,
+      msg: "",
+      first: "",
+      last: "",
+      mail: "",
+      purpose: "",
+      nameRules: [v => !!v || "Name is required"],
+      emailRules: [
+        v => !!v || "Email is required",
+        v => /.+@.+/.test(v) || "Invalid Email"
+      ],
+      textRules: [
+        v => !!v || "Please write something",
+        v => v != undefined && v.length > 10 || "That's too short"
+      ]
+    };
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        this.$refs.form.reset();
+        this.msg = "Successsfully submitted!"
+      } else {
+          this.msg = "Invalid details!"
+      }
+      this.snackbar = true;
+    },
+    reset() {
+        this.$refs.form.reset();
+    }
+  }
+};
+</script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,28 +1,26 @@
 <template>
   <v-container>
-      <v-window v-model="onboarding" vertical>
-        <v-window-item v-for="project in projects" :key="project.name">
-            <v-row>
-              <v-col cols="12">
-                <v-card height="75vh" elevation="12">
-                  <div class="d-flex flex-no-wrap justify-space-between">
-                    <div>
-                      <v-card-title>{{ project.name }}</v-card-title>
-                      <v-card-subtitle>{{ project.text }}</v-card-subtitle>
-                      <v-card-text>
-                        <div>{{ project.text }}</div>
-                        <div class="subtitle">{{ project.tools }}</div>
-                      </v-card-text>
-                    </div>
-
-                    <v-img :src="project.image"></v-img>
-                  </div>
-                </v-card>
-              </v-col>
-            </v-row>
-        </v-window-item>
-      </v-window>
-    
+    <v-window v-model="onboarding" vertical>
+      <v-window-item v-for="project in projects" :key="project.name">
+        <v-row>
+          <v-col cols="12">
+            <v-card height="75vh" elevation="12">
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title>{{ project.name }}</v-card-title>
+                  <v-card-subtitle>{{ project.text }}</v-card-subtitle>
+                  <v-card-text>
+                    <div>{{ project.text }}</div>
+                    <div class="subtitle">{{ project.tools }}</div>
+                  </v-card-text>
+                </div>
+                <v-img :src="project.image"></v-img>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-window-item>
+    </v-window>
 
     <v-card-actions class="justify-space-between">
       <v-btn fab x-small @click="prev">
@@ -47,26 +45,25 @@
 <script>
 export default {
   data: () => ({
-    
     onboarding: 0,
     projects: [
-        {
-          name: "Sprint",
-          image: "/project1.jpg",
-          text: "A text editor for competitive programmers."
-        },
-        {
-          name: "Personal Portfolio",
-          image: "/project2.jpg",
-          text: "Personal Website for showcasing myself."
-        },
-        {
-          name: "Sandbox",
-          image: "/project3.jpg",
-          text: "A tool for proctured coding assessments."
-        }
-      ],
-      length: 10,
+      {
+        name: "Sprint",
+        image: "/project1.jpg",
+        text: "A text editor for competitive programmers."
+      },
+      {
+        name: "Personal Portfolio",
+        image: "/project2.jpg",
+        text: "Personal Website for showcasing myself."
+      },
+      {
+        name: "Sandbox",
+        image: "/project3.jpg",
+        text: "A tool for proctured coding assessments."
+      }
+    ],
+    length: 10
   }),
 
   methods: {
